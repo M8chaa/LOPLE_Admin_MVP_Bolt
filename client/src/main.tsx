478263b0b6d6f74b493005e160f3faa5ea95c98d
M8chaa <110661830+M8chaa@users.noreply.github.com>
2025-05-29 11:00:06 +0900

    Fix client rendering

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <CssBaseline />
       <App />
     </BrowserRouter>
   </React.StrictMode>
